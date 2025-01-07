e79360554c2689d3dbf936ef5aa13e91d4ef9297
JhieKander <taduranjo.4869@gmail.com>
2025-01-08 04:01:15 +0800

    OpenWeatherMap - Getting the weather using the city name.

---
+++ src/weather/weather.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Query, Param } from '@nestjs/common';
+import { WeatherService } from './weather.service';
+
+@Controller('weather')
+export class WeatherController {
+  constructor(private readonly weatherService: WeatherService) {}
+
+  // Endpoint for getting current weather by city name.
+  @Get('current')
+  async getCurrentWeather(@Query('city') city: string) {
+    return this.weatherService.getCurrentWeather(city);
+  }
+}
