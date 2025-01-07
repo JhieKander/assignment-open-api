8e65afb9fb6e015ca582800e54fe2088a19e0bf4
JhieKander <taduranjo.4869@gmail.com>
2025-01-08 03:57:33 +0800

    NestJS Backend (OpenWeatherMap)

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { WeatherService } from './weather/weather.service';
+import { WeatherController } from './weather/weather.controller';
+
+@Module({
+  imports: [HttpModule],
+  controllers: [WeatherController],
+  providers: [WeatherService],
+})
+export class AppModule {}
